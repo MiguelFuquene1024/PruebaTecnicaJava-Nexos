c5b2ff28b8549347e9880992677b8c8a05880c90
MiguelFuquene1024 <migueplay_10@hotmail.com>
2021-12-28 08:23:23 -0500

    Initial Commit

---
+++ src/main/java/com/eci/pruebatecnica/automotiveinventory/AutomotiveinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.eci.pruebatecnica.automotiveinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveinventoryApplication.class, args);
+	}
+
+}
